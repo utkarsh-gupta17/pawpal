ed6ef37fe751e802d24c3963326e50a9053fdc12
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-09-19 19:25:22 +0530

    integrating firebase

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import {useUser} from '@clerk/clerk-expo'
 
@@ -7,10 +7,35 @@ const Header = () => {
   const {user} = useUser();
 
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }}>
+      <View>
+        <Text style={styles.mainText}>Welcome,</Text>
+        <Text style={styles.userName}>{user?.fullName}</Text>
+      </View>
+      <Image source={{uri:user?.imageUrl}} style={styles.userImage} />
     </View>
   )
 }
 
 export default Header
+
+const styles = StyleSheet.create({
+  mainText: {
+    fontFamily: 'inter',
+    fontSize: 18,
+  },
+  userName: {
+    fontFamily: 'monteserrat-bold',
+    fontSize: 25,
+  },
+  userImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 99
+  },
+})
---
+++ config/FirebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "pawpal-5b835.firebaseapp.com",
+  projectId: "pawpal-5b835",
+  storageBucket: "pawpal-5b835.appspot.com",
+  messagingSenderId: "250584598404",
+  appId: "1:250584598404:web:eec89e2715815c84a3bfd1",
+  measurementId: "G-45M96DCYKL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+// const analytics = getAnalytics(app);
