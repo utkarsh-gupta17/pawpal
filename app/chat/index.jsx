3907be2058348bce17a49ddd397db6e9bce856c0
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-09-24 01:08:54 +0530

    chat working completely but not form

--- app/chat/index.jsx
+++ app/chat/index.jsx
@@ -1,19 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams, useNavigation } from 'expo-router'
-import { doc,getDoc } from 'firebase/firestore'
+import { addDoc, collection, doc,getDoc, onSnapshot } from 'firebase/firestore'
 import {db} from './../../config/FirebaseConfig'
-import { useUser } from '@clerk/clerk-expo';
+import { useUser } from '@clerk/clerk-expo'
+import { GiftedChat } from 'react-native-gifted-chat'
+import moment from 'moment'
+
 
 const Chat = () => {
 
   const {user} = useUser();
   const params = useLocalSearchParams();
   const navigation = useNavigation();
+  const [messages, setMessages] = useState([])
+
 
   useEffect(() => {
     GetUserDetails();
+
+    const unsubscribe = onSnapshot(collection(db,'Chat',params?.id,'Messages'),(snapshot)=>{
+      const messageData = snapshot.docs.map((doc)=>({
+        _id:doc.id,
+        ...doc.data(),
+      }))
+      setMessages(messageData);
+    });
+
+    return ()=>unsubscribe();
   }, [])
+
+
+  const onSend = async(messages) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+    messages[0].createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    await addDoc(collection(db,'Chat',params.id,'Messages'),messages[0])
+  }
   
   const GetUserDetails = async()=>{
     const docRef = doc(db,'Chat',params?.id);
@@ -32,9 +56,16 @@ const Chat = () => {
   }
 
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <GiftedChat
+    messages={messages}
+    onSend={messages => onSend(messages)}
+    showUserAvatar={true}
+    user={{
+      _id: user?.primaryEmailAddress?.emailAddress,
+      name:user?.fullName,
+      avatar:user?.imageUrl,
+    }}
+  />
   )
 }
 
