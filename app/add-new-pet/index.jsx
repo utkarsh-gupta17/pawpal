93193161485bf7b72b36efb56199268aa85f5d4a
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-09-23 23:57:27 +0530

    code for add new pet with ui, but not working completely =(

--- app/add-new-pet/index.jsx
+++ app/add-new-pet/index.jsx
@@ -1,20 +1,48 @@
-import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { useNavigation } from 'expo-router';
+import { useNavigation, useRouter } from 'expo-router';
 import Colors from '../../constants/Colors';
 import {Picker} from '@react-native-picker/picker';
+import { collection, getDocs, setDoc } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
+import * as ImagePicker from 'expo-image-picker';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import storage from './../../config/FirebaseConfig';
+import { useUser } from '@clerk/clerk-expo';
+
 
 const AddNewPet = () => {
 
   const navigation = useNavigation();
 
-  const [formData, setFormData] = useState(second)
+  const [formData, setFormData] = useState({
+    category:'Dogs',sex:"Male"
+  });
+  const [gender, setGender] = useState();
+  const [categoryList,setCategoryList] = useState([]);
+  const [selectedCategory,setSelectedCategory] = useState();
+  const [image,setImage] = useState();
+  const { user } = useUser();
+  const [loader,setLoader] = useState(false);
+  const router = useRouter();
+
 
   useEffect(()=>{
     navigation.setOptions({
       headerTitle: 'Add New Pet'
     })
+    GetCategories();
   },[]);
+  
+
+  const GetCategories = async()=>{
+    setCategoryList([]);
+    const snapshot = await getDocs(collection(db,'Category'));
+    snapshot.forEach((doc)=>{
+      // console.log(doc.data());
+      setCategoryList(categoryList=>[...categoryList,doc.data()]);
+    })
+  }
 
   const handleInputChange = (fieldName,fieldValue)=>{
     // console.log(fieldName,fieldValue);
@@ -24,14 +52,92 @@ const AddNewPet = () => {
     }))
   }
 
+  const onSubmit = ()=>{
+    console.log(formData);
+    if(Object.keys(formData).length!=8){
+      ToastAndroid.show('Enter All Details',ToastAndroid.TOP);
+      return;
+    }
+    UploadImage();
+  }
+  
+  const UploadImage = async()=>{
+    setLoader(true);
+    const res = await fetch(image);
+    const blobImage = await res.blob();
+    const storageRef = ref(storage,'PetAdopt'+Date.now()+'.jpg');
+
+    uploadBytes(storageRef,blobImage).then((snapshot)=>{
+      console.log('File Uploaded');
+    }).then(resp=>{
+      getDownloadURL(storageRef).then(async(downloadUrl)=>{
+        console.log(downloadUrl);
+        SaveFormData(downloadUrl);
+      })
+    });
+  }
+
+  const SaveFormData = async(imageUrl)=>{
+    const docId = Date.now().toString();
+    await setDoc(doc(db,'Pets',docId),{
+      ...formData,
+      imageUrl:imageUrl,
+      userName:user?.fullName,
+      userEmail:user?.primaryEmailAddress.emailAddress,
+      userImage:user?.imageUrl,
+      id:docId,
+    });
+    setLoader(false);
+    router.replace('/(tabs)/home');
+  }
+
+
+  const imagePicker = async ()=>{
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+
   return (
     <ScrollView style={{padding:20}}>
       <Text>AddNewPet</Text>
-      <Image source={require('./../../assets/images/paw.png')} style={styles.pawImage} />
+      <Pressable onPress={imagePicker}>
+        {
+          !image ? <Image source={require('./../../assets/images/paw.png')} style={styles.pawImage} />
+          : <Image source={{uri:image}} style={styles.pawImage} />
+        }
+      </Pressable>
 
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Pet Name*</Text>
         <TextInput style={styles.input} onChangeText={(value)=>handleInputChange('name',value)}/>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Pet Category*</Text>
+        <Picker
+          selectedValue={gender}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) => {
+            setSelectedCategory(itemValue);
+            handleInputChange('sex',itemValue);
+          }}>
+          {
+            categoryList.map((category,index)=>(
+              <Picker.Item key={index} label={category.name} value={category.name} />
+            ))
+          }
+        </Picker>
       </View>
 
       <View style={styles.inputContainer}>
@@ -41,21 +147,26 @@ const AddNewPet = () => {
 
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Age*</Text>
-        <TextInput style={styles.input} onChangeText={(value)=>handleInputChange('age',value)}/>
+        <TextInput keyboardType='number-pad' style={styles.input} onChangeText={(value)=>handleInputChange('age',value)}/>
       </View>
 
-      <Picker
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)
-        }>
-        <Picker.Item label="Male" value="Male" />
-        <Picker.Item label="Female" value="Female" />
-      </Picker>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Gender*</Text>
+        <Picker
+          selectedValue={gender}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) => {
+            setGender(itemValue);
+            handleInputChange('sex',itemValue);
+          }}>
+          <Picker.Item label="Male" value="Male" />
+          <Picker.Item label="Female" value="Female" />
+        </Picker>
+      </View>
 
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Weight*</Text>
-        <TextInput style={styles.input} onChangeText={(value)=>handleInputChange('weight',value)}/>
+        <TextInput keyboardType='number-pad' style={styles.input} onChangeText={(value)=>handleInputChange('weight',value)}/>
       </View>
 
       <View style={styles.inputContainer}>
@@ -68,12 +179,12 @@ const AddNewPet = () => {
         <TextInput numberOfLines={5} multiline={true} style={styles.input} onChangeText={(value)=>handleInputChange('about',value)}/>
       </View>
 
-      <TouchableOpacity style={styles.submit}>
-        <Text style={{fontFamily:'inter',textAlign:'center'}}>Submit</Text>
+      <TouchableOpacity style={styles.submit} onPress={onSubmit} disabled={loader}>
+        {loader
+          ? <ActivityIndicator size={'large'}/>
+          : <Text style={{fontFamily:'inter',textAlign:'center'}}>Submit</Text>
+        }
       </TouchableOpacity>
-
-      <View style={{height:70}}></View>   
-      {/* the above is for getting extra height below */}
 
     </ScrollView>
   )
@@ -107,5 +218,6 @@ const styles = StyleSheet.create({
     marginVertical:15,
     backgroundColor:Colors.PRIMARY,
     borderRadius:8,
+    marginBottom:50,
   }
 })
